f12ad01c987584d37ab173cc6c1408750f2139ee
Serhii Hryniuk <serhiibhryniuk@gmail.com>
2026-06-09 19:22:21 +0300

    feat: add CharacterListView for filtering characters by name

--- rick_and_morty/urls.py
+++ rick_and_morty/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from rick_and_morty.views import get_random_character_view
+from rick_and_morty.views import get_random_character_view, CharacterListView
 
 app_name = "rick_and_morty"
 
@@ -9,5 +9,10 @@ urlpatterns = [
         "characters/random/",
         get_random_character_view,
         name="random_character"
+    ),
+    path(
+        "characters/",
+        CharacterListView.as_view(),
+        name="characters"
     )
 ]
--- rick_and_morty/views.py
+++ rick_and_morty/views.py
@@ -1,6 +1,6 @@
 import random
 
-from rest_framework import status
+from rest_framework import status, generics
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -16,3 +16,14 @@ def get_random_character_view(request: Request) -> Response:
     random_character = Character.objects.get(pk=random_primary_key)
     serializer = CharacterSerializer(random_character)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class CharacterListView(generics.ListAPIView):
+    serializer_class = CharacterSerializer
+
+    def get_queryset(self):
+        queryset = Character.objects.all()
+        name = self.request.query_params.get("name")
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+        return queryset
